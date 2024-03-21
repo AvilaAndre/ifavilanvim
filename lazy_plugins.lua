9dc8cedf6fad65373eb18490db633cc410da83b3
André Ávila <75994325+AvilaAndre@users.noreply.github.com>
2024-03-21 16:37:48 +0000

    Add files via upload

---
+++ lazy_plugins.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		require("ifavila.plugins.telescope")
+	},
+	{ 
+		"nvim-treesitter/nvim-treesitter",
+		{ run = ':TSUpdate' },
+		enabled = false
+	},
+	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+	{
+	    'windwp/nvim-autopairs',
+	    event = "InsertEnter",
+	    config = true
+	    -- use opts = {} for passing setup options
+	    -- this is equalent to setup({}) function
+	},
+	{
+		require("ifavila.plugins.lsp")
+	},
+}
