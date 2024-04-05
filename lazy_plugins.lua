86f589744691c21b4fdccb1aba92c899416aea8f
AvilaAndre <avila_andre14@hotmail.com>
2024-04-05 21:17:47 +0100

    trouble plugin added

--- lazy_plugins.lua
+++ lazy_plugins.lua
@@ -1,17 +1,18 @@
 return {
 	require("ifavila.plugins.telescope"),
 	require("ifavila.plugins.treesitter"),
-	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
 	{
-	    "windwp/nvim-autopairs",
-	    event = "InsertEnter",
-	    config = true
-	    -- use opts = {} for passing setup options
-	    -- this is equalent to setup({}) function
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true
+		-- use opts = {} for passing setup options
+		-- this is equalent to setup({}) function
 	},
-    require("ifavila.plugins.mason"),
+	require("ifavila.plugins.mason"),
 	require("ifavila.plugins.lspconfig"),
 	require("ifavila.plugins.lsp-zero"),
 	require("ifavila.plugins.modicator"),
-	require("ifavila.plugins.todo-comments")
+	require("ifavila.plugins.todo-comments"),
+	require("ifavila.plugins.trouble")
 }
