a0808333e36bdb2e01dae23d5ad040b9f9820ae7
AvilaAndre <avila_andre14@hotmail.com>
2024-05-26 14:05:12 +0100

    show tabs

--- set.lua
+++ set.lua
@@ -6,7 +6,7 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.expandtab = true
+vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
@@ -20,4 +20,4 @@ vim.opt.scrolloff = 8
 vim.g.mapleader = " "
 
 vim.opt.list = true
-vim.opt.listchars = { space = '·' }
+vim.opt.listchars = { space = '·', tab = '⭾ ' }
