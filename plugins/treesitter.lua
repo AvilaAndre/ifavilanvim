dac9b71012e889834fd2a9fe8d0d4db0b3622fd3
AvilaAndre <avila_andre14@hotmail.com>
2024-07-04 12:52:18 +0100

    enable treesitter highlight by default

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -2,5 +2,12 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	run = ':TSUpdate',
 	-- enabled = false
-	config = true
+	config = function()
+		local configs = require 'nvim-treesitter.configs'
+		configs.setup {
+			highlight = {
+				enable = true
+			},
+		}
+	end,
 }
