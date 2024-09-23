17b0f910ca4cd40b7c3a5fe27e65514b46bea8c8
AvilaAndre <avila_andre14@hotmail.com>
2024-09-23 15:15:46 +0100

    added markdown and metals plugins

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -7,7 +7,6 @@ return {
             { "<leader>pf", "<cmd>Telescope find_files<cr>",  desc = "Find Project Files" },
             { "<C-p>",      "<cmd>Telescope git_files<cr>",   desc = "Git Files" },
             { "<leader>ps", "<cmd>Telescope grep_string<cr>", desc = "Grep in File" },
-
         }
     end,
     lazy = false
