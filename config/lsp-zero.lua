b71e0de90e7be4d773ee795ee78541e526c712e4
AvilaAndre <avila_andre14@hotmail.com>
2024-11-12 18:38:34 +0000

    add ruff to pylsp

--- config/lsp-zero.lua
+++ config/lsp-zero.lua
@@ -23,3 +23,32 @@ require('mason-lspconfig').setup({
 vim.diagnostic.config({
 	signs = false
 })
+
+require("lspconfig")["pylsp"].setup({
+	settings = {
+		pylsp = {
+			plugins = {
+				ruff = {
+					-- formatter + Linter + isort
+					enabled = true,
+					extendSelect = { "I" },
+				},
+				-- formatter options
+				black = { enabled = false },
+				autopep8 = { enabled = false },
+				yapf = { enabled = false },
+				-- linter options
+				pylint = { enabled = false, executable = "pylint" },
+				pyflakes = { enabled = false },
+				pycodestyle = { enabled = false },
+				-- type checker
+				pylsp_mypy = { enabled = true },
+				mypy = { enabled = true },
+				-- auto-completion options
+				jedi_completion = { fuzzy = true },
+				-- import sorting
+				pyls_isort = { enabled = false },
+			},
+		},
+	},
+})
