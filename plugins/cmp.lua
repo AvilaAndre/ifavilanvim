55e844d3ce0bd3b633672b1a46eff0dbb105d408
AvilaAndre <avila_andre14@hotmail.com>
2024-12-19 19:07:40 +0000

    disable cmdline cmp

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -41,19 +41,19 @@ return {
             })
 
             -- `:` cmdline setup.
-            cmp.setup.cmdline(':', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = 'path' }
-                }, {
-                    {
-                        name = 'cmdline',
-                        option = {
-                            ignore_cmds = { 'Man', '!' }
-                        }
-                    }
-                })
-            })
+            -- cmp.setup.cmdline(':', {
+            -- mapping = cmp.mapping.preset.cmdline(),
+            -- sources = cmp.config.sources({
+            -- { name = 'path' }
+            -- }, {
+            -- {
+            -- name = 'cmdline',
+            -- option = {
+            -- ignore_cmds = { 'Man', '!' }
+            -- }
+            -- }
+            -- })
+            -- })
 
             cmp.setup.filetype({ 'sql' }, {
                 sources = {
