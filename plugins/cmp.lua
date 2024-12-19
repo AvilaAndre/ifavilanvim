44d8c44444345a5b2122163f6d21d972d5fa1160
AvilaAndre <avila_andre14@hotmail.com>
2024-12-19 11:48:14 +0000

    changed cmp config

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -40,28 +40,19 @@ return {
                 },
             })
 
-
-            -- `/` cmdline setup.
-            cmp.setup.cmdline({ '/', '?' }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = 'buffer' }
-                }
-            })
-
             -- `:` cmdline setup.
             cmp.setup.cmdline(':', {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-            { name = 'path' }
-            }, {
-            {
-            name = 'cmdline',
-            option = {
-            ignore_cmds = { 'Man', '!' }
-            }
-            }
-            })
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    {
+                        name = 'cmdline',
+                        option = {
+                            ignore_cmds = { 'Man', '!' }
+                        }
+                    }
+                })
             })
 
             cmp.setup.filetype({ 'sql' }, {
